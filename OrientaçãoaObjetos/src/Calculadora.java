4c84043047c461d7dfbc35b31bbef763ea36515d
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-04-04 10:26:07 -0300

    Add files via upload

---
+++ OrientaçãoaObjetos/src/Calculadora.java
@@ -0,0 +1,29 @@
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		double n1 = 5;
+		double n2 = 2;
+		double resulSub;
+		double resulSo;
+		double resulMu;
+		double resulDiv;
+		
+
+		resulSo = n1 + n2;
+		System.out.println("O Resultado da soma é " + resulSo);
+		
+		resulSub = n1 - n2;
+		System.out.println("O Resultado da subtração é " + resulSub);
+		
+		resulMu = n1 * n2;
+		System.out.println("O Resultado da multiplicação é " + resulMu);
+		
+		resulDiv = n1 / n2;
+		System.out.println("O Resultado da divisão é " + resulDiv);
+		
+		
+	}
+
+}
