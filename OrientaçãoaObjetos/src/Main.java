f2bfd40ab6975475a0c5260ac953df3a8462a60a
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-04-11 10:59:56 -0300

    Add files via upload

---
+++ OrientaçãoaObjetos/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Pessoas pessoa1 = new Pessoas();
+		
+		pessoa1.nome = "Sara";
+		pessoa1.numFigurinhas = 1;
+		pessoa1.receber(13);
+		System.out.println("Nome: " + pessoa1.nome);
+		System.out.println("Número de Figurinhas: " + pessoa1.numFigurinhas);
+		
+		
+		
+		
+		
+		}
+			 
+		
+	}
+
+
