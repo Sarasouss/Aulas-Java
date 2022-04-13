f58e6b9b4ec5ffaf0d2ea684ab0e137e10778170
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-04-13 08:03:15 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAObjeto/src/ManipulçãoDeObjetos.java
@@ -0,0 +1,20 @@
+
+public class ManipulçãoDeObjetos {
+
+	public static void main(String[] args) {
+		
+		pessoa pessoa1 = new pessoa (); //criando variavel e instanciando o objeto
+		
+		
+		pessoa1.nome = "Gustavo";  //dando valor a variavel
+			pessoa1.numfigurinha = 1;
+		      pessoa1.receber(5);
+
+					System.out.println(pessoa1.nome);
+						System.out.println(pessoa1.numfigurinha);
+		
+						
+
+}
+	
+}
---
+++ ProgramaçãoOrientadaAObjeto/src/pessoa.java
@@ -0,0 +1,16 @@
+
+public class pessoa {
+	String nome;
+	int numfigurinha;
+	
+	void receber (int numfigurinha) {
+		this.numfigurinha += numfigurinha;
+		
+	}
+	
+	void entregar () {
+	
+		
+	}
+
+}
