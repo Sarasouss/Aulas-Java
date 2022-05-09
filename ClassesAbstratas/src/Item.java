65752059538d03d234711dc1dfdee8a45c328a54
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-05-09 11:43:07 -0300

    09/05

---
+++ ClassesAbstratas/src/Item.java
@@ -0,0 +1,30 @@
+
+public abstract class Item {
+	
+	private int posX;
+	private int posy;
+	
+	public void pegar() {
+		
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosy() {
+		return posy;
+	}
+
+	public void setPosy(int posy) {
+		this.posy = posy;
+	}
+	
+	
+}
+
+
