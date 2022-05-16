f1135006dd464073197db27b3093ff223236deed
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-05-16 11:31:40 -0300

    16/05

---
+++ ContaBancaria2/src/ContaInvestimento.java
@@ -0,0 +1,32 @@
+
+public class ContaInvestimento extends ContaBancaria{
+	
+	private double saldo = 0;
+	
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	
+	@Override
+	public void depositar(double valor) {
+		// TODO Auto-generated method stub
+		saldo +=valor;
+	}
+
+	@Override
+	public void sacar(double valor) {
+		// TODO Auto-generated method stub
+		saldo -=valor;
+	}
+
+
+	@Override
+	public double CalcularSaldo() {
+		// TODO Auto-generated method stub
+		return saldo+(saldo*0.05);
+	}
+
+	
+}
---
+++ ContaBancaria2/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+	public static void main(String[] args) {
+		//Na minha aplicação eu deduzi que as contas investimento e corrente faziam parte de uma mesma conta bancária, ou seja
+		//a pessoa que fosse dona de uma conta bancária teria que necessariamente ter uma conta investimento e uma conta corrente
+		//por conta disso, a conta investimento inicia com saldo nulo, e a conta corrente inicia com o saldo igual ao da conta corrente.
+		
+		ContaBancaria ContaCorrente = new ContaCorrente();
+		ContaBancaria ContaInvestimento = new ContaInvestimento();
+		
+		System.out.printf("O saldo na conta corrente é equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
+		System.out.printf("O saldo na conta investimento é equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
+			
+		ContaCorrente.depositar(200);
+		ContaInvestimento.depositar(200);
+		System.out.printf("O saldo na conta corrente é equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
+		System.out.printf("O saldo na conta investimento é equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
+		
+		ContaCorrente.sacar(100);
+		ContaInvestimento.sacar(100);
+		System.out.printf("O saldo na conta corrente é equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
+		System.out.printf("O saldo na conta investimento é equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
+		
+		ContaInvestimento.trasnferir(200, ContaCorrente);
+		System.out.printf("O saldo na conta corrente é equivalente a: R$%.2f%n", ContaCorrente.CalcularSaldo());
+		System.out.printf("O saldo na conta investimento é equivalente a: R$%.2f%n%n", ContaInvestimento.CalcularSaldo());
+		
+		
+		
+	}
+
+}
