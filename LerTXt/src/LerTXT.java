fd18178c36cfa71799b4f1d89a3768d9f80b82f7
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-05-25 10:25:54 -0300

    25/05

---
+++ LerTXt/src/LerTXT.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LerTXT {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+		try(BufferedReader reader = new BufferedReader(new FileReader("arquivo.txt"))){
+			
+			String line;
+			String string = "";
+			
+			while((line = reader.readLine())!= null) {
+				string += line + "\n";
+						
+			}
+			 System.out.println(string);
+		}
+
+	
+	}
+
+}
