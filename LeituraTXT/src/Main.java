7c703b48818251439d56c3e6278f2eb61475622f
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-05-30 11:38:04 -0300

    30/05

--- LeituraTXT/src/Main.java
+++ LeituraTXT/src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -6,32 +8,50 @@ import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException  {
-	
-		
+	public static void main(String[] args) throws IOException {
+
 		List<Sabonete> lista = new ArrayList<Sabonete>();
-		
+//		Primeiro cria a lista para receber os
+//		metodos da Classe sabonete
+
 		Sabonete sabonete1 = new Sabonete("Flores", "Rosa");
+		Sabonete sabonete2 = new Sabonete("Amaderado ", " Marrom");
+		Sabonete sabonete3 = new Sabonete("Frutas Citricas ", " Vermelho");
+//		Depois adiciono os parametros do metodo Sabonete ex: cheiro e cor
+
 		lista.add(sabonete1);
-		
-		Sabonete sabonete2 = new Sabonete("Amadeirado", "Marrom");
 		lista.add(sabonete2);
-		
-		Sabonete sabonete3 = new Sabonete("Frutas Citricas", "Vermelho");
 		lista.add(sabonete3);
-		 			
-		
-		try(BufferedWriter escrever = new BufferedWriter(new FileWriter("sabonete.txt"))){
-		
-			for(Sabonete sabonete : lista)
-				
-		escrever.write(sabonete.toString());		
+//		Depois coloco eles dentro da lista
+
+		try (BufferedWriter escrever = new BufferedWriter(new FileWriter("Sabonete.txt",true))) {
+			for (Sabonete sabonete : lista) {
+				escrever.write(sabonete.toString());
+			}
+		}
+//		Já o true após o txt é para não subescrever o texto
+//		Ja aqui transformaFmos todo o conteudo da lista em txt
+//		com o metodo BufferedWriter e o for
+
+		List<Sabonete> lista2 = new ArrayList<Sabonete>();
+//		Depois criamos uma nova lista para receber o conteudo do txt que iremos ler 
+
+		try (BufferedReader reader = new BufferedReader(new FileReader("Sabonete.txt"))) {
+//		Criamos o metodo BufferredReader para ler o txt Sabonete 
+			String line;
+//		Criamos uma String para receber o conteudo lido	
+			while ((line = reader.readLine()) != null) {
+				Sabonete sabonete = new Sabonete(line);
+				lista2.add(sabonete);
+			}
+//		Depois criamos um objeto sabonete  que recebe o que esta dentro de line
+//		E depois adicionamos sabonete a lista
+			for (Sabonete sabonete : lista2) {
+				System.out.println(sabonete);
+			}
+//		O for foi feito para interagir com a lista, para poder mexer em uma variavel por vez.
+//		Depois foi adicionado um syso para mostrar o conteudo de sabonete na tela
+
 		}
 	}
-
-	
-		
-		
-	}
-
-
+}
--- LeituraTXT/src/Sabonete.java
+++ LeituraTXT/src/Sabonete.java
@@ -1,26 +1,30 @@
 
 public class Sabonete {
 
-	 private String cheiro;
-	 private String cor;
-	 
-		@Override
-		public String toString() {
-			return " Sabonete cheiro: " + cheiro + ", cor: " + cor + "\n" ;
-		}
+	private String cheiro;
+	private String cor;
 
+	@Override
+	public String toString() {
+		return " Sabonete cheiro: " + cheiro + ", cor: " + cor + "\n";
+	}
+
+// O toString vai mostrar o conteúdo que aparecera na tela quando chamarmos sabonete 
 	public Sabonete(String cheiro, String cor) {
 		super();
 		this.cheiro = cheiro;
 		this.cor = cor;
 	}
+
+//	Aqui foi criado o contrutor, entao quando chamarmos sabonete poderemos adicionar dois parametros.
 	public Sabonete(String texto) {
-		
-		String[]  arrayAuxiliar1 = texto.split(",");
-		String[]  arrayAuxiliar2 = arrayAuxiliar1[0].split(":");		
-		this.cheiro = arrayAuxiliar2[1].trim();		
-		
-		String[]  arrayAuxiliar3 = arrayAuxiliar1[0].split(":");
+		String[] arrayAuxiliar1 = texto.split(",");
+		String[] arrayAuxiliar2 = arrayAuxiliar1[0].split(":");
+		this.cheiro = arrayAuxiliar2[1].trim();
+
+		String[] arrayAuxiliar3 = arrayAuxiliar1[0].split(":");
 		this.cor = arrayAuxiliar3[1].trim();
+//	Esse novo contrutor  faz a quebra dos conteúdos recebidos pelo txt, conforme o que queremos
+//	slipt serve para quebrar o texto e trim para tirar os espaços
 	}
 }
