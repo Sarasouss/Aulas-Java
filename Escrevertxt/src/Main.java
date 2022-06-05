b4b6739e30ff4f17cf9db1dc642e5bf782cb6997
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-06-04 21:28:09 -0300

    comentário 04/06

--- Escrevertxt/src/Main.java
+++ Escrevertxt/src/Main.java
@@ -7,10 +7,12 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		String s =  "Texto para ser Gravado";
+// criando a variavel com o valor que sera escrito
 		
 		try(BufferedWriter escrever = new BufferedWriter(new FileWriter("saida.txt"))){
-		
-		escrever.write(s);		
+// Chamando o metodo da escrita e nomeando o txt		
+		escrever.write(s);
+// executando o que sera escrito
 		}
 	}
 
