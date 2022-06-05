04f1af5cf3a121caa867586cb990395b6bdaa5df
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-06-04 21:25:20 -0300

    comentário 04/06

--- CódigosDiversos/src/Main.java
+++ CódigosDiversos/src/Main.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner entrada = new Scanner(System.in);
 		System.out.print("Qual número você quer multiplicar? ");
+// chamasse um scanner para receber o valor desejado
 		int y= entrada.nextInt();
-		
+// criamos uma váriavel para receber o valor do scanner
 		for(int x=0; x<=10; x++) {
-			System.out.printf("%d x %d = %d%n", y, x, y*x);
+
+// Realiza um for com o valor digitado e criasse uma tabela de multiplicação até 1
+			System.out.printf( y + "x" +x + "=" +  y*x);
+// syso criado para mostrar o resultado da tabuada
 		}
 
 	}
