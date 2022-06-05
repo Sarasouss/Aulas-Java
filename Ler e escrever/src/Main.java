e39677c75cd26f55c32d81ae6e7b38dc41c3ce81
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-06-04 21:30:27 -0300

    comentário 04/06

--- Ler e escrever/src/Main.java
+++ Ler e escrever/src/Main.java
@@ -11,27 +11,30 @@ public class Main {
 		
 		Scanner entrada = new Scanner(System.in);
 		String mensagem = "";
-		
+		// criando o objeto de scanner
+		// e criano uma string onde ficara guardado a mensagem digitada
 		
 
 		try(BufferedWriter escrever = new BufferedWriter(new FileWriter("mensagem.txt"))){
-			
+		//chamando o metodo escrever com a criacao do txt
 		System.out.println("Digite sua mensagem ");
 			mensagem = entrada.nextLine();
+		//pedindo a digitacao da mensagem
 			escrever.write(mensagem);	
-			
+		//guardando ela no txt
 		}
 	try(BufferedReader reader = new BufferedReader(new FileReader("mensagem.txt"))){
-			
+		// chamando o metodo leitura com o txt da escrita	
 			String line;
 			String string = "";
-			
+		//criando onde ela sera guardada	
 			while((line = reader.readLine())!= null) {
+		// a string vai receber tudo ate que o txt esteja null ou seja coma linha vazia
 				string += line + "\n";
-						
+			// \n serve para pular linha quando mostrar na tela			
 			}
 			 System.out.println(string);
-		
+		// syso para mostrar conteúdo lido
 		
 	}
 
