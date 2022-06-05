334c3f9d9da7feba63dcf45aa3d13e013c523eb9
Sara Jesus de sousa <102962799+Sarasouss@users.noreply.github.com>
2022-06-04 21:31:00 -0300

    comentário 04/06

--- LerTXt/src/LerTXT.java
+++ LerTXt/src/LerTXT.java
@@ -8,15 +8,16 @@ public class LerTXT {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		
 		try(BufferedReader reader = new BufferedReader(new FileReader("arquivo.txt"))){
-			
+		// BufferedReader serve para ler o arquivo escolhido
 			String line;
 			String string = "";
-			
+			// String onde a informacao sera guardada
 			while((line = reader.readLine())!= null) {
 				string += line + "\n";
-						
+			// a string vai receber tudo ate que o txt esteja null ou seja coma linha vazia
 			}
 			 System.out.println(string);
+			 //visualizacao da informacao
 		}
 
 	
